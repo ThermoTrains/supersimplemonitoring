4d7100ced0cf5039f6d4adcbff87f2280e5bc93b
Sebastian Häni <haeni.sebastian@gmail.com>
2017-10-25 23:06:32 +0200

    add simple file logging

--- api/ping/index.php
+++ api/ping/index.php
@@ -1,5 +1,25 @@
 <?php
-$inputJSON = file_get_contents('php://input');
-$stats = json_decode($inputJSON, TRUE); //convert JSON into array
+$stats = file_get_contents("php://input");
+$stats = json_encode(json_decode($stats));
+$timestamp = date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']);
+$content = preg_replace("/\r|\n/", "", $stats);
 
-echo json_encode($stats);
+$line = "[" . $timestamp . "] " . $content . "\n";
+
+$file = "./../../data/stats.txt";
+
+// Prepend line to file
+$handle = fopen($file, "r+");
+$len = strlen($line);
+$final_len = filesize($file) + $len;
+$cache_old = fread($handle, $len);
+rewind($handle);
+$i = 1;
+
+while (ftell($handle) < $final_len) {
+    fwrite($handle, $line);
+    $line = $cache_old;
+    $cache_old = fread($handle, $len);
+    fseek($handle, $i * $len);
+    $i++;
+}
--- index.php
+++ index.php
@@ -1,31 +1,3 @@
-<!doctype html>
-<html>
-    <head>
-    <meta charset="utf-8"/>
-     <title>Super Simple Monitoring</title>
-    </head>
-    <body>
-        <main>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Computer</th>
-                        <th>Time</th>
-                        <th>CPU</th>
-                        <th>Memory</th>
-                        <th>Disk</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>No data recorded yet</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-        </main>
-    </body>
-</html>
+<pre><code><?php
+$file = file_get_contents("data/stats.txt");
+echo $file;
