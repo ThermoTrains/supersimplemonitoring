869d99fec9e935200f35cd7ae00fa5173cda8fe1
Sebastian Häni <haeni.sebastian@gmail.com>
2017-11-04 15:28:06 +0100

    improve read out script

--- ping.ps1
+++ ping.ps1
@@ -1,18 +1,33 @@
-while (1) {
-    $hostname = hostname
-    $proc = (Get-WmiObject -class win32_processor -EA SilentlyContinue | Measure-Object -property LoadPercentage -Average | Select Average | % {$_.Average / 100})
-    $mem = Get-WmiObject win32_OperatingSystem -EA SilentlyContinue 
-    $mem = (($mem.TotalVisibleMemorySize - $mem.FreePhysicalMemory) / $mem.TotalVisibleMemorySize)
-    $disk = Get-PSDrive C | Select-Object Free | ForEach-Object {$_.Free}
+<#
+.SYNOPSIS
+    Collects a bunch of monitoring data and sends it to a remote REST API server.
+#>
 
-    $stats = @{
-        "hostname" = $hostname
-        "cpu"      = $proc
-        "memory"   = $mem
-        "disk"     = $disk
-    } | ConvertTo-Json
+# For this to work, Open Hardware Monitor needs to be running on the system.
+# Download here: http://openhardwaremonitor.org/
 
-    Invoke-WebRequest -Uri https://monitoring.sebastianhaeni.ch/api/ping/ -Method Post -Body $stats
-    
-    Start-Sleep -Seconds 300 # 5 Minutes
-}
+$temp = Get-WmiObject -Namespace "root/OpenHardwareMonitor" Sensor `
+    | Where-Object {$_.SensorType -eq 'Temperature' -and $_.Name -like 'CPU Core #*'} `
+    | Select-Object Value `
+    | ForEach-Object {$_.Value}
+$proc = Get-WmiObject -Namespace "root/OpenHardwareMonitor" Sensor `
+    | Where-Object {$_.SensorType -eq 'Load' -and $_.Name -like 'CPU Core #*'} `
+    | Select-Object Value `
+    | ForEach-Object {$_.Value / 100}
+$mem  = Get-WmiObject -Namespace "root/OpenHardwareMonitor" Sensor `
+    | Where-Object {$_.SensorType -eq 'Load' -and $_.Name -eq 'Memory'} `
+    | Select-Object Value `
+    | ForEach-Object {$_.Value / 100}
+$disk = Get-PSDrive C `
+    | Select-Object Free `
+    | ForEach-Object {$_.Free}
+
+$stats = @{
+    "hostname" = hostname
+    "cpu" = $proc
+    "temperatures" = $temp
+    "memory" = $mem
+    "disk" = $disk
+} | ConvertTo-Json
+
+Invoke-WebRequest -Uri https://monitoring.yourcompany.com/api/ping/ -Method Post -Body $stats
