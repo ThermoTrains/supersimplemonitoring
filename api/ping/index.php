3f9a6f9ac3db179ee579ba05801147b3da17fea2
Sebastian Häni <haeni.sebastian@gmail.com>
2018-01-09 23:41:47 +0100

    send email when disk is running out of space

--- api/ping/index.php
+++ api/ping/index.php
@@ -3,6 +3,14 @@ $stats = file_get_contents("php://input");
 $stats = json_decode($stats);
 $timestamp = date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']);
 $stats->timestamp = $timestamp;
+
+if (property_exists($stats, 'disk') && is_numeric($stats->disk)) {
+    $gigabytesLeft = $stats->disk / 1024 / 1024 / 1024;
+    if ($gigabytesLeft < 5) {
+        mail('haeni.sebastian@gmail.com', "Thermobox only has $gigabytesLeft GiB left of disk space", 'Act quickly!');
+    }
+}
+
 $stats = json_encode($stats);
 $content = preg_replace("/\r|\n/", "", $stats) . "\n";
 
